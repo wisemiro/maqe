9ee23b8d1765df9c99522180d46f85ab7a90ecc9
wycemiro <wycemiro2@gmail.com>
2021-12-08 11:02:02 +0300

    logic on if-else, switch & date formatting

--- lib/nav-bar/home/ui/home.dart
+++ lib/nav-bar/home/ui/home.dart
@@ -1,5 +1,4 @@
-import 'dart:convert';
-
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
@@ -41,7 +40,7 @@ class _HomeState extends State<Home> {
                   scrollDirection: Axis.vertical,
                   itemCount: state.holidayModel.leaveRequests.length,
                   itemBuilder: (context, index) {
-                    return _cardData(context, index,
+                    return _cardData(context, state,
                         state.holidayModel.leaveRequests[index]);
                   }),
             );
@@ -53,12 +52,21 @@ class _HomeState extends State<Home> {
     ));
   }
 
-  _cardData(BuildContext context, index, LeaveRequest leaveRequest) {
+  _cardData(BuildContext context, state, LeaveRequest leaveRequest) {
     Intl.defaultLocale = 'en_US';
     var month = leaveRequest.requestList[0].date;
     final DateFormat formatter = DateFormat('dd MMM yyyy');
     final String frmtd = formatter.format(month);
-  
+
+    var firstDate = leaveRequest.requestList.first.date;
+    var lastDate = leaveRequest.requestList.last.date;
+
+    final DateFormat lastDayformatter = DateFormat('dd');
+    final String lastDay = lastDayformatter.format(lastDate);
+
+    final DateFormat firstDayformatter = DateFormat('dd MMM yyyy');
+    final String firstDay = firstDayformatter.format(firstDate);
+
     return Card(
       elevation: 0,
       shape: RoundedRectangleBorder(
@@ -71,12 +79,22 @@ class _HomeState extends State<Home> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TitleText(
-              inputText:
-                  "${leaveRequest.requestList[0].type.replaceAll(RegExp(r'_'), '')}",
-              color: MaqeColor.titleColor,
-              size: 22,
-            ),
+            if (leaveRequest.requestList[0].type == "remote" ||
+                leaveRequest.requestList[0].type == "personal_leave" ||
+                leaveRequest.requestList[0].type == "sick_leave") ...[
+              TitleText(
+                inputText: "Leave",
+                color: MaqeColor.titleColor,
+                size: 22,
+              ),
+            ] else if (leaveRequest.requestList[0].type ==
+                "switch_holiday") ...[
+              TitleText(
+                inputText: "Switch",
+                color: MaqeColor.titleColor,
+                size: 22,
+              ),
+            ],
             SizedBox(
               height: MediaQuery.of(context).size.height * .02,
             ),
@@ -89,7 +107,25 @@ class _HomeState extends State<Home> {
                       WidgetSpan(
                         child: Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 2.0),
-                          child: Icon(Icons.desktop_mac_sharp),
+                          child: (() {
+                            switch (leaveRequest.requestList[0].type) {
+                              case 'remote':
+                                return Icon(Icons.desktop_mac_sharp);
+                              case 'personal_leave':
+                                return RotatedBox(
+                                  quarterTurns: 3,
+                                  child: Icon(
+                                    Icons.flight_outlined,
+                                  ),
+                                );
+                              case 'switch_holiday':
+                                return Icon(
+                                  Icons.shuffle,
+                                );
+                              case 'sick_leave':
+                                return Icon(Icons.medical_services);
+                            }
+                          }()),
                         ),
                       ),
                       TextSpan(
@@ -105,26 +141,30 @@ class _HomeState extends State<Home> {
                   text: TextSpan(
                     children: [
                       WidgetSpan(
-                        child: Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 2.0),
-                            child: (() {
-                              print(leaveRequest.status);
-                              if (leaveRequest.status.toString() == 1) {
-                                Icon(
-                                  Icons.access_time_filled,
-                                  color: MaqeColor.orangeColor,
-                                );
-                              } else if (leaveRequest.status == "approved") {
-                                Icon(
-                                  Icons.circle_sharp,
-                                  color: MaqeColor.orangeColor,
-                                );
-                              }
-                            }())),
-                      ),
+                          child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                        child: (() {
+                          switch (leaveRequest.status) {
+                            case 'pending':
+                              return Icon(
+                                Icons.access_time_filled,
+                                color: MaqeColor.orangeColor,
+                              );
+                            case 'approved':
+                              return Icon(
+                                Icons.check_circle,
+                                color: MaqeColor.greenColor,
+                              );
+                            case 'canceled':
+                              return Icon(
+                                Icons.cancel_rounded,
+                                color: MaqeColor.lightGreyColor,
+                              );
+                          }
+                        }()),
+                      )),
                       TextSpan(
-                        text: '${leaveRequest.status}',
+                        text: '${leaveRequest.status.toCapitalized()}',
                         style: TextStyle(
                           color: MaqeColor.subTitleColor,
                         ),
@@ -134,28 +174,28 @@ class _HomeState extends State<Home> {
                 )
               ],
             ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * .02,
-            ),
-            RichText(
-              text: TextSpan(
-                children: [
-                  WidgetSpan(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 2.0),
-                      child: RotatedBox(
-                          quarterTurns: 1, child: Icon(Icons.flight_outlined)),
+            if (leaveRequest.requestList.length > 1) ...[
+              RichText(
+                text: TextSpan(
+                  children: [
+                    WidgetSpan(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                        child: RotatedBox(
+                            quarterTurns: 1,
+                            child: Icon(Icons.flight_outlined)),
+                      ),
                     ),
-                  ),
-                  TextSpan(
-                    text: ' 28-29 Mar 2021',
-                    style: TextStyle(
-                      color: MaqeColor.subTitleColor,
+                    TextSpan(
+                      text: '$lastDay - $firstDay',
+                      style: TextStyle(
+                        color: MaqeColor.subTitleColor,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
+            ],
           ],
         ),
       ),
@@ -164,7 +204,7 @@ class _HomeState extends State<Home> {
 
   _headBody() {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -180,9 +220,6 @@ class _HomeState extends State<Home> {
                 Icons.calendar_today,
                 color: MaqeColor.blueColor,
               ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .01,
-              ),
               SubTitleText(
                   inputText: "Public holidays",
                   color: MaqeColor.blueColor,
@@ -193,4 +230,14 @@ class _HomeState extends State<Home> {
       ),
     );
   }
+}
+
+extension StringCasingExtension on String {
+  String toCapitalized() =>
+      this.length > 0 ? '${this[0].toUpperCase()}${this.substring(1)}' : '';
+  String get toTitleCase => this
+      .replaceAll(RegExp(' +'), ' ')
+      .split(" ")
+      .map((str) => str.toCapitalized())
+      .join(" ");
 }
