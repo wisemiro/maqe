19c634357c0e2e34c1dcfaab84b03ac0a9224034
wycemiro <wycemiro2@gmail.com>
2021-12-08 15:31:51 +0300

    top data,extension,colors

---
+++ lib/extension/extension.dart
@@ -0,0 +1,9 @@
+extension StringCasingExtension on String {
+  String toCapitalized() =>
+      this.length > 0 ? '${this[0].toUpperCase()}${this.substring(1)}' : '';
+  String get toTitleCase => this
+      .replaceAll(RegExp(' +'), ' ')
+      .split(" ")
+      .map((str) => str.toCapitalized())
+      .join(" ");
+}
--- lib/nav-bar/home/ui/home.dart
+++ lib/nav-bar/home/ui/home.dart
@@ -4,9 +4,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:maqe/nav-bar/home/blocs/bloc/read_json_bloc.dart';
 import 'package:maqe/nav-bar/home/models/model.dart';
-import 'package:maqe/nav-bar/home/ui/subs/top-data.dart';
 import 'package:maqe/theme/colors.dart';
 import 'package:maqe/theme/style.dart';
+import 'package:maqe/extension/extension.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -18,23 +18,22 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-      children: [
-        TopData(),
-        _headBody(),
-        BlocBuilder<ReadJsonBloc, ReadJsonState>(builder: (context, state) {
-          if (state is ReadJsonLoading) {
-            return CircularProgressIndicator();
-          }
-          if (state is ReadJsonError) {
-            return Center(
-              child: Text("$state.error"),
-            );
-          }
-          if (state is ReadJsonSuccess) {
-            return Container(
-              height: MediaQuery.of(context).size.height * 0.6,
+    return BlocBuilder<ReadJsonBloc, ReadJsonState>(builder: (context, state) {
+      if (state is ReadJsonLoading) {
+        return CircularProgressIndicator();
+      }
+      if (state is ReadJsonError) {
+        return Center(
+          child: Text("$state.error"),
+        );
+      }
+      if (state is ReadJsonSuccess) {
+        return Column(
+          children: [
+            _topData(state.holidayModel),
+            _headBody(),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.5,
               child: ListView.builder(
                   padding: EdgeInsets.zero,
                   scrollDirection: Axis.vertical,
@@ -43,13 +42,12 @@ class _HomeState extends State<Home> {
                     return _cardData(context, state,
                         state.holidayModel.leaveRequests[index]);
                   }),
-            );
-          }
-
-          return CircularProgressIndicator.adaptive();
-        }),
-      ],
-    ));
+            ),
+          ],
+        );
+      }
+      return CircularProgressIndicator.adaptive();
+    });
   }
 
   _cardData(BuildContext context, state, LeaveRequest leaveRequest) {
@@ -58,12 +56,11 @@ class _HomeState extends State<Home> {
     final DateFormat formatter = DateFormat('dd MMM yyyy');
     final String frmtd = formatter.format(month);
 
-    var firstDate = leaveRequest.requestList.first.date;
     var lastDate = leaveRequest.requestList.last.date;
-
     final DateFormat lastDayformatter = DateFormat('dd');
     final String lastDay = lastDayformatter.format(lastDate);
 
+    var firstDate = leaveRequest.requestList[0].date;
     final DateFormat firstDayformatter = DateFormat('dd MMM yyyy');
     final String firstDay = firstDayformatter.format(firstDate);
 
@@ -230,14 +227,189 @@ class _HomeState extends State<Home> {
       ),
     );
   }
-}
 
-extension StringCasingExtension on String {
-  String toCapitalized() =>
-      this.length > 0 ? '${this[0].toUpperCase()}${this.substring(1)}' : '';
-  String get toTitleCase => this
-      .replaceAll(RegExp(' +'), ' ')
-      .split(" ")
-      .map((str) => str.toCapitalized())
-      .join(" ");
-}
+  _topData(HolidayModel holidayModel) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * .3,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  stops: [0.5, 0.5],
+                  colors: [MaqeColor.lightblueColor, Colors.transparent],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              20,
+            ),
+          ),
+          margin: EdgeInsets.symmetric(horizontal: 10),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                //card title
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TitleText(
+                      inputText: "My holiday",
+                      color: MaqeColor.titleColor,
+                      size: 22,
+                      fontWeight: FontWeight.bold,
+                    )
+                  ],
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //total data
+                      Column(
+                        children: [
+                          TitleText(
+                            inputText: '${holidayModel.totalDay}',
+                            color: MaqeColor.titleColor,
+                            size: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          SubTitleText(
+                            inputText: "Total",
+                            color: MaqeColor.subTitleColor,
+                            size: 18,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ],
+                      ),
+                      //used data
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: MediaQuery.of(context).size.width * .05,
+                        ),
+                        margin: EdgeInsets.symmetric(
+                            horizontal:
+                                MediaQuery.of(context).size.width * .05),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            right: BorderSide(
+                              color: MaqeColor.lightGreyColor,
+                            ),
+                            left: BorderSide(
+                              color: MaqeColor.lightGreyColor,
+                            ),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            TitleText(
+                              inputText: '${holidayModel.totalDayUsed}',
+                              color: MaqeColor.blueColor,
+                              size: 22,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            SubTitleText(
+                              inputText: "Used",
+                              color: MaqeColor.subTitleColor,
+                              size: 18,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ],
+                        ),
+                      ),
+                      //left data
+                      Column(
+                        children: [
+                          TitleText(
+                            inputText: '${holidayModel.totalDayLeft}',
+                            color: MaqeColor.orangeColor,
+                            size: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          SubTitleText(
+                            inputText: "Left",
+                            color: MaqeColor.subTitleColor,
+                            size: 18,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+
+                //buttons
+                _buttonSection(),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  _buttonSection() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextButton.icon(
+          onPressed: () {},
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(MaqeColor.blueColor),
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  10.0,
+                ),
+              ),
+            ),
+          ),
+          icon: Icon(
+            Icons.add,
+            color: MaqeColor.whiteColor,
+          ),
+          label: RegularText(
+            inputText: "Leave",
+            size: 16,
+            color: MaqeColor.whiteColor,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * .02,
+        ),
+        OutlinedButton.icon(
+          onPressed: () {},
+          icon: Icon(
+            Icons.shuffle, //font awesome pro random icon - nil
+            color: MaqeColor.blueColor,
+          ),
+          style: OutlinedButton.styleFrom(
+              side: BorderSide(color: MaqeColor.blueColor),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  10.0,
+                ),
+              )),
+          label: RegularText(
+            inputText: "Switch",
+            color: MaqeColor.blueColor,
+            size: 16,
+            fontWeight: FontWeight.normal,
+          ),
+        )
+      ],
+    );
+  }
+}//end
